32cc8ba7ccf6d6a1177f73be3ab45ccabbef860d
oakwoodgates <funkyoaktree@gmail.com>
2016-07-05 23:47:20 -0400

    moved fb pixel code to plugin

--- mtoll.php
+++ mtoll.php
@@ -608,3 +608,22 @@ function mtoll_woocommerce_edit_account_form_start() {
 	</p>
 	<?php
 }
+
+add_action( 'wp_head', 'mtoll_fb_pixel_wp_head' );
+function mtoll_fb_pixel_wp_head() {
+	?>
+	<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+	document,'script','https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '367652926691662');
+	fbq('track', "PageView");</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=367652926691662&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+	<?php
+}
